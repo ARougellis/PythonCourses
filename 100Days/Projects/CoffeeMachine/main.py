cc7bf05b3fa7bd88071405115b36db808def05c2
AlexRougellis <arougellis@linksquares.com>
2023-02-22 16:49:08 -0500

    Moved variables over to its own file. Then called upon them in main.py

--- 100Days/Projects/CoffeeMachine/main.py
+++ 100Days/Projects/CoffeeMachine/main.py
@@ -1,39 +1,8 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-default_resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
+from variables import MENU, default_resources
 
 # --->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->--->
 # FUNCTIONS
-#
+
 
 def restock(available_resources, baseline_resources):
     """
