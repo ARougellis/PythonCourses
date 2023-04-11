8c896dc09c2b645d7c98ee5825c6f5e3913afb39
AlexRougellis <arougellis@linksquares.com>
2023-04-11 18:32:38 -0400

    More work done with *args and **kwargs

--- 100Days/CourseWork/day-27-args-and-kwargs/main.py
+++ 100Days/CourseWork/day-27-args-and-kwargs/main.py
@@ -9,4 +9,21 @@ label = tkinter.Label(text="This is a test label", font=("Ariel", 18, "italic"))
 label.pack()
 
 
+# Button
+
+# Create an event to get the button to work
+def button_clicked():
+    new_text = inputs.get()
+    label["text"] = new_text
+
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry
+inputs = tkinter.Entry(width=15)
+inputs.pack()
+
+
+
 window.mainloop()
