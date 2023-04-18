200413226a1bf0933fa8df8b731f45a981bea9c5
AlexRougellis <arougellis@linksquares.com>
2023-04-18 18:26:00 -0400

    Pack, place, grid methods

--- 100Days/CourseWork/day-27-args-and-kwargs/main.py
+++ 100Days/CourseWork/day-27-args-and-kwargs/main.py
@@ -12,18 +12,27 @@ window.title("Title Placeholder")
 window.minsize(width=600, height=400)
 
 # LABEL
-label = Label(text="This is a test label", font=("Ariel", 18, "italic"))
+label = Label(text="This is a test label", font=("Ariel", 18, "bold"))
 label.config(text="new_text")
-label.pack()
+# label.pack()
+# label.place(x=0, y=0)
+label.grid()
+
 
 # BUTTON
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack()
+# button.place(x=300, y=200)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button", command=button_clicked)
+new_button.grid(column=2, row=0)
 
 # ENTRY
 input = Entry(width=10)
 print(input.get())
-input.pack()
+# input.pack()
+input.grid(column=3, row=2)
 
 
 
