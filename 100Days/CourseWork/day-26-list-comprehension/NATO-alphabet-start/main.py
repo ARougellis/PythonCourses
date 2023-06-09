56db888b42a34b0e3be445d3625d36829e1a5fd5
AlexRougellis <arougellis@linksquares.com>
2023-06-09 16:45:26 -0400

    Improve the nato phonetic alphabet code thing with try:except

--- 100Days/CourseWork/day-26-list-comprehension/NATO-alphabet-start/main.py
+++ 100Days/CourseWork/day-26-list-comprehension/NATO-alphabet-start/main.py
@@ -13,8 +13,15 @@ nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_df.
 
 # Step 2:
 #   Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Enter a word: ").upper()
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        code_words_list = [nato_alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the English alphabet please.")
+        generate_phonetic()
+    else:
+        print(code_words_list)
 
-code_words_list = [nato_alphabet_dict[letter] for letter in user_word]
 
-print(code_words_list)
+generate_phonetic()
