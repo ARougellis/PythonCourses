7f4dec7e732291cc780112bcb73433b1ed8896e2
AlexRougellis <arougellis@linksquares.com>
2023-07-25 14:24:37 -0400

    almost done with the day's lesson

--- 100Days/CourseWork/day-33-api-endpoints/iss_overhead_notifier/main.py
+++ 100Days/CourseWork/day-33-api-endpoints/iss_overhead_notifier/main.py
@@ -1,15 +1,37 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+MY_LAT = 42.389788  # 42.38978760538564
+MY_LNG = -71.144877  # -71.14487654232931
 
-data = response.json()
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# timestamp = data['timestamp']
+# longitude = data['iss_position']['latitude']
+# latitude = data['iss_position']['latitude']
+#
+# position = (longitude, latitude)
+#
+# print(f"time: {timestamp}")
+# print(f"position: {position}")
 
-timestamp = data['timestamp']
-longitude = data['iss_position']['latitude']
-latitude = data['iss_position']['latitude']
+sun_params = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
 
-position = (longitude, latitude)
+sun_response = requests.get(url="https://api.sunrise-sunset.org/json", params=sun_params)
+sun_response.raise_for_status()
+sun_data = sun_response.json()
 
-print(f"time: {timestamp}")
-print(f"position: {position}")
+sunrise = sun_data['results']['sunrise'].split("T")[1].split(":")[0]
+sunset = sun_data['results']['sunset'].split("T")[1].split(":")[0]
+
+time_now = datetime.now()
+
+print(sun_data)
+
